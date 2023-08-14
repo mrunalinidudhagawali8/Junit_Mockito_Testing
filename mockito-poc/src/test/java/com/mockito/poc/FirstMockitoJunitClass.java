b2f31c2d81da5055ca3bc7023d8c842b07a5f1c7
Mrunalini Dudhagawali <dudhagawali@gmail.com>
2023-08-14 23:20:36 +0530

    adding project skeleton

---
+++ mockito-poc/src/test/java/com/mockito/poc/FirstMockitoJunitClass.java
@@ -0,0 +1,14 @@
+package com.mockito.poc;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class FirstMockitoJunitClass {
+
+	@Test
+	public void testTest()
+	{
+		assertTrue(true);
+	}
+}
